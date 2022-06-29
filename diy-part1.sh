ab3cd014d3071779443f5e68125d3c060b219684
coutanz <32047880+coutanz@users.noreply.github.com>
2022-06-29 12:07:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,14 +10,14 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+ Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 pushd package
 rm -rf luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 popd
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+ Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
